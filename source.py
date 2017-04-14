5192d7f6c0a89bf4b84c1ca2e8add2db4daf44e3
hsywhu <530706463@qq.com>
2017-04-14 10:31:20 +0800

    v1.1

--- source.py
+++ source.py
@@ -10,12 +10,21 @@ finally:
 soup=BeautifulSoup(file_text,"html.parser")
 div=soup.find(id="test-editormd-view2")
 div=str(div).replace("\n"," ")
+
+#对DIV内容进行预处理，删除【实验步骤】前的内容，删除"`"符号
+position=re.search("实验步骤",div,flags=0).span()[1]
+div=div[position:].replace("`","")
+
 output=open('output.txt','w')
 output.write('')
 output.close()
+count=1
 output=open('output.txt','a',encoding='utf-8')
 for text in re.findall(r"\d\.\d.*?<center>", div):
 	text=text[:-8].replace("\t","")
-	print(text)
-	output.write(text+'\n')
+	position=re.search(r"\d.\d+ ?",text,flags=0).span()[1]
+	text=text[position:]
+	print(str(count)+" "+text+"\n")
+	output.write(str(count)+' '+text+'\n'+'\n')
+	count=count+1
 output.close()
